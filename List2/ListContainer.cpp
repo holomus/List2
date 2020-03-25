177eff9bc4a2d670ad32da225b9c092352b97388
Sanjar Akhmedov <leoabrahaam@gmail.com>
2020-03-25 16:14:03 +0500

    Trying to understand how list should work, added additionalmethods for NodeIterator class

--- List2/ListContainer.cpp
+++ List2/ListContainer.cpp
@@ -2,7 +2,7 @@
 
 
 
-void* ListContainer::NodeIterator::getElement(size_t & size)
+void* ListContainer::NodeIterator::getElement(size_t& size)
 {
 	size = node->elem_size;
 	return node->elem;
@@ -16,10 +16,7 @@ bool ListContainer::NodeIterator::hasNext()
 void ListContainer::NodeIterator::goToNext()
 {
 	if (hasNext()) {
-		node->elem = node->next->elem;
-		node->elem_size = node->next->elem_size;
-		node->prev = node->next->prev; //next->prev == current
-		node->next = node->next->next;
+		node = node->next;
 	}
 	else throw NextElementError();
 }
@@ -27,13 +24,76 @@ void ListContainer::NodeIterator::goToNext()
 bool ListContainer::NodeIterator::equals(Iterator* right)
 {
 	NodeIterator* iterator = dynamic_cast<NodeIterator*>(right);
-	return iterator != nullptr && node == iterator->node;
+	return iterator != nullptr && node->isSame(*iterator->node);
+}
+
+bool ListContainer::NodeIterator::hasEqual(Node &element)
+{
+	return node->isEqual(element);
+}
+
+ListContainer::NodeIterator& ListContainer::NodeIterator::operator++()
+{
+	goToNext();
+	return *this;
+}
+
+ListContainer::NodeIterator ListContainer::NodeIterator::operator++(int)
+{
+	NodeIterator copy(*this);
+	operator++();
+	return copy;
 }
 
 
 ListContainer::Iterator* ListContainer::newIterator() 
 {
-	//is it what it is supposed to do 
+	//is it what it is supposed to do
 	//I dont understand
 	return new NodeIterator;
+}
+
+int ListContainer::size() {
+
+}
+
+size_t ListContainer::max_bytes()
+{
+	return size_t();
+}
+
+
+ListContainer::Iterator* ListContainer::find(void *elem, size_t size)
+{
+	NodeIterator* iterator = nullptr;
+	for (auto cur = static_cast<NodeIterator*>(begin()); *cur != *static_cast<NodeIterator*>(end()); ++(*cur)) {
+		if (cur->hasEqual(Node(elem, size))) {
+
+		}
+	}
+	return iterator;
+}
+
+//returns iterator to the position after last element
+ListContainer::Iterator* ListContainer::end() {
+	return (front_sentry == back_sentry) ? nullptr : new NodeIterator(&back_sentry);
+}
+
+
+//returns iterator to the first element
+ListContainer::Iterator* ListContainer::begin() {
+	return (front_sentry == back_sentry) ? nullptr : new NodeIterator(front_sentry.next);
+}
+
+void ListContainer::remove(Iterator * iter)
+{
+}
+
+void ListContainer::clear()
+{
+}
+
+
+bool ListContainer::empty() {
+	return front_sentry == back_sentry;
 }
--- List2/ListContainer.h
+++ List2/ListContainer.h
@@ -3,13 +3,17 @@
 class ListContainer :	public AbstractList2
 {
 public:
-	ListContainer(MemoryManager &mem) : AbstractList2(mem) {};
-	~ListContainer() {};
+	ListContainer(MemoryManager &mem) : AbstractList2(mem) {
+		front_sentry.next = &back_sentry;
+		back_sentry.prev = &front_sentry;
+		};
+	~ListContainer() {
+		clear();
+	};
 	int size() override;
 	virtual size_t max_bytes() override;
 	Iterator* find(void *elem, size_t size) override;
 	Iterator* begin() override;
-	Iterator* newIterator() override;
 	Iterator* end() override;
 	void remove(Iterator *iter) override;
 	void clear() override;
@@ -21,21 +25,55 @@ public:
 protected:
 	struct Node
 	{
+		Node(Node *next, Node *prev, void *elem, size_t size) : next(next), prev(prev), elem(elem), elem_size(size) {};
+		Node(void *elem, size_t size) : Node(nullptr, nullptr, elem, size) {};
+		Node() :Node(nullptr, nullptr, nullptr, 0) {};
+		~Node() {
+			delete elem;
+			elem = nullptr;
+		}
 		Node *next;
 		Node *prev;
 		void *elem;
 		size_t elem_size;
+		bool isEqual(Node &right) {
+			return elem == right.elem && elem_size == right.elem_size;
+		}
+		bool isSame(Node &right) {
+			return prev == right.prev && next == right.next;
+		};
 	};
-
+	
 	class NodeIterator : public ListContainer::Iterator 
 	{
 	public:
+		NodeIterator(Node *node) : node(node) {};
+		NodeIterator() : NodeIterator(nullptr) {};
+		~NodeIterator() {
+			delete node;
+			node = nullptr;
+		}
 		void* getElement(size_t &size) override;
 		bool hasNext() override;
 		void goToNext() override;
 		bool equals(Iterator *right) override;
+
+		bool hasEqual(Node &right);
+
+		NodeIterator& operator++();
+		NodeIterator operator++(int);
+		friend bool operator==(NodeIterator &left, NodeIterator &right) { return left.equals(&right); };
+		friend bool operator!=(NodeIterator &left, NodeIterator &right) { return !left.equals(&right); };
+	protected:
+		void setNode(Node *node) { this->node = node; };
+		Node* getNode() { return node; };
 	private:
+		friend ListContainer::ListContainer(MemoryManager &mem);
 		Node *node;
 	};
+private:
+	Node front_sentry;
+	Node back_sentry;
+	Iterator* newIterator() override;
 };
 
